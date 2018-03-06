16a578dcbe879eda42391f34af9b94429f3c7be1
student <student@US-Lab-18.local>
2018-03-06 12:28:50 -0500

    begin natural merge sort

---
+++ naturalMergeSort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void Merge(int *A,int *L,int leftCount,int *R,int rightCount) {
+	int i,j,k;
+
+	// i - to mark the index of left aubarray (L)
+	// j - to mark the index of right sub-raay (R)
+	// k - to mark the index of merged subarray (A)
+	i = 0; j = 0; k =0;
+
+	while(i<leftCount && j< rightCount) {
+		if(L[i]  < R[j])
+    {
+      A[k++] = L[i++];
+    } else {
+      A[k++] = R[j++];
+    }
+	}
+	while(i < leftCount)  {A[k++] = L[i++];}
+	while(j < rightCount) {A[k++] = R[j++];}
+}
+
+//basic mergesort function. I will be comparing my natural merge sort to this
+void MergeSort(int *A,int n) {
+	int mid,i, *L, *R;
+	if(n < 2){return;} // base condition. If the array has less than two element, do nothing.
+
+	mid = n/2;  // find the mid index.
+
+	// create left and right subarrays
+	// mid elements (from index 0 till mid-1) should be part of left sub-array
+	// and (n-mid) elements (from mid to n-1) will be part of right sub-array
+	L = (int*)malloc(mid*sizeof(int));
+	R = (int*)malloc((n- mid)*sizeof(int));
+
+	for(i = 0;i<mid;i++) L[i] = A[i]; // creating left subarray
+	for(i = mid;i<n;i++) R[i-mid] = A[i]; // creating right subarray
+
+	MergeSort(L,mid);  // sorting the left subarray
+	MergeSort(R,n-mid);  // sorting the right subarray
+	Merge(A,L,mid,R,n-mid);  // Merging L and R into A as sorted list.
+
+  free(L);//we no longer need to reserve the memory for L and R
+  free(R);
+}
+
+void NaturalMergeSort(int *Array, int len){
+
+  bool run = true;
+
+  for (int i = 0; i < len; i++){
+
+  }
+
+}
+
+//TODO: implement linked lists and use those for the merges because I will only need to be accessing the first element of it or appending to the end which is O(1) just like with arrays. This I will have unlimited length runs. I can then merge the runs faster b/c joining two lists is easy (just changing one pointer)
+
+int main(){
+  //
+}
